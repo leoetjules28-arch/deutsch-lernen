9e11efa651e162d993e7e821330e02a340ef9c96
leoetjules28-arch <leoetjules28@gmail.com>
2026-03-15 19:34:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "deutsch-lernen-v1";
+
+// When the app is installed, cache everything
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Delete old caches when a new version is activated
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// When the app requests a file, serve it from cache if possible
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        // Save new files into the cache as they are loaded
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => {
+        // If offline and not cached, return the main page
+        return caches.match("/index.html");
+      });
+    })
+  );
+});
